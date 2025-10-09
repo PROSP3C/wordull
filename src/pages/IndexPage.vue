c410f7fb13e62fef41f439de6511decb25c04771
Declan Possnett <declanpossnett@gmail.com>
2025-10-09 16:44:33 +0100

    add win and lose dialogs with the option to see the board on win or lose

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,12 +5,26 @@
   import LetterRow from '@/components/LetterRow.vue'
   import LetterSquare from '@/components/LetterSquare.vue'
   import { GameState } from '@/enums'
+  import { computed, ref } from 'vue'
 
-  const { guesses, keys, gameState } = storeToRefs(useGameLogicStore())
+  const { guesses, keys, gameState, currentRowGuessIndex } =
+    storeToRefs(useGameLogicStore())
+
+  const seeBoardFromDialog = ref(false)
+  const hasWon = computed(
+    () => gameState.value === GameState.Won && !seeBoardFromDialog.value,
+  )
+  const hasLost = computed(
+    () => gameState.value === GameState.Lost && !seeBoardFromDialog.value,
+  )
 
   const onStartGame = () => {
     const gameLogicStore = useGameLogicStore()
     gameLogicStore.startGame()
+  }
+
+  const onSeeBoard = () => {
+    seeBoardFromDialog.value = true
   }
 </script>
 
@@ -28,7 +42,7 @@
       />
     </div>
     <div
-      v-if="gameState === GameState.Playing"
+      v-else
       class="flex column justify-evenly items-center"
     >
       <div class="flex column q-gutter-sm">
@@ -48,28 +62,59 @@
 
       <KeyboardWrapper :keys="keys" />
     </div>
-    <div
-      v-if="gameState === GameState.Lost"
-      class="flex column justify-center items-center"
+    <q-dialog v-model="hasLost">
+      <div class="IndexPage-dialog flex column justify-center items-center">
+        <h2>You Lost...</h2>
+        <p>Better luck next time.</p>
+        <q-btn
+          color="primary"
+          label="New Game"
+          icon-right="restart_alt"
+          @click="onStartGame()"
+        />
+        <q-btn
+          color="secondary"
+          label="See Board"
+          icon-right="visibility"
+          style="margin-top: 1rem"
+          @click="onSeeBoard()"
+        />
+      </div>
+    </q-dialog>
+    <q-dialog v-model="hasWon">
+      <div class="IndexPage-dialog flex column justify-center items-center">
+        <h2>Well Done!</h2>
+        <p>
+          You solved Wordull in {{ currentRowGuessIndex + 1 }} guess{{
+            currentRowGuessIndex + 1 > 1 ? 'es' : ''
+          }}!
+        </p>
+        <q-btn
+          color="primary"
+          label="New Game"
+          icon-right="restart_alt"
+          @click="onStartGame()"
+        />
+        <q-btn
+          color="secondary"
+          label="See Board"
+          icon-right="visibility"
+          style="margin-top: 1rem"
+          @click="onSeeBoard()"
+        />
+      </div>
+    </q-dialog>
+    <q-dialog
+      v-model="seeBoardFromDialog"
+      position="top"
     >
-      <h1>You Lost...</h1>
       <q-btn
         color="primary"
-        icon-right="restart_alt"
-        @click="onStartGame()"
-      />
-    </div>
-    <div
-      v-if="gameState === GameState.Won"
-      class="flex column justify-center items-center"
-    >
-      <h1>You Won!</h1>
-      <q-btn
-        color="primary"
-        icon-right="restart_alt"
-        @click="onStartGame()"
-      />
-    </div>
+        label="See Stats"
+        icon-right="info"
+        @click="seeBoardFromDialog = false"
+      ></q-btn>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -83,6 +128,13 @@
       font-size: 4rem;
       margin-bottom: 1rem;
       font-weight: bold;
+    }
+
+    &-dialog {
+      background: #111;
+      padding: 1rem;
+      border-radius: 4px;
+      color: #fff;
     }
   }
 </style>
